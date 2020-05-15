7d5d41b992429cf5a5be318fb465f4aff1184445
Anirudh Emmadi <kanna6501@gmail.com>
2020-05-15 12:36:12 -0500

    Changed path for imports

--- src/production/scrape.py
+++ src/production/scrape.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import .functions.data as data
+from .data import *
 import os
 
 # Configure chromedriver
