c97fef13f9c587767a3532ffc937633e71b7bf1c
Anirudh Emmadi <kanna6501@gmail.com>
2020-06-14 12:55:24 -0500

    Finished grade endpoint

--- src/production/render.py
+++ src/production/render.py
@@ -1,5 +1,6 @@
 # Import Libraries
 import requests
+import re
 import sys
 
 # Renders raw markdown from README.md in github master branch into HTML
@@ -13,15 +14,58 @@ def docs_html():
                          'Content-Type': 'text/x-markdown'})
     return f'<html><head><title>Unofficial API for UT Dallas Coursebook</title></head><body>{html.text}</body></html>'
 
+# Retrieves grades from utd-grades github repo
+
 
 def get_grades(term, course, section):
-    url = 'https://api.github.com/repos/bharatari/utd-grades/'
+    # Format to match data json
     term = convert_term(term)
-    return term
+    course = convert_course(course)
+
+    # Bad input
+    if(term == -1):
+        return 400
+
+    # Find json for term
+    url = f'https://api.github.com/repos/bharatari/utd-grades/contents/data/{term}?ref=master'
+    response = requests.get(url).json()
+
+    for raw_file in response:
+        if "json" in raw_file["name"]:
+            url = raw_file["download_url"]
+
+    # Grab json
+    grade_data = requests.get(url).json()
+
+    # Parse and return requested data
+    for i in range(len(grade_data)):
+        if grade_data[i]["subj"].lower() == course[0].lower():
+            if grade_data[i]["num"] == course[1]:
+                if grade_data[i]["sect"].lower() == section.lower():
+                    return grade_data[i]
+
+
+# Converts course into an array seperating subject and number, math2413 -> [math, 2413]
+
+def convert_course(course):
+    num = re.findall('\d+', course)[0]
+    subj = course.replace(f"{num}", "")
+    return [subj, num]
+
+# Converts term into expanded format, 18f -> Fall 2018
 
 
 def convert_term(term):
     season = term[-1]
-    term = term[:-1]
-    print(season, file=sys.stderr)
-    return [season, term]
+    term = f"20{term[:-1]}"
+
+    if season == "f":
+        season = "Fall"
+    elif season == "s":
+        season = "Spring"
+    elif season == "u":
+        season = "Summer"
+    else:
+        return -1
+
+    return f"{season}%20{term}"
