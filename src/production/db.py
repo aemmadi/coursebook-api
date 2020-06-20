3b699161e237cdaa2c79e328adaff723fb5a21af
Anirudh Emmadi <kanna6501@gmail.com>
2020-06-20 18:39:47 -0500

    Implemented db for grades

---
+++ src/production/db.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import os
+# from .render import grades_by_term
+# from .data import *
+
+from data import *
+from render import grades_by_term
+from dotenv import load_dotenv
+load_dotenv()
+
+db = MongoClient(os.environ.get("DB_KEY"))
+grades_db = db.grades
+
+
+def get_single_course_grade(term, course, section):
+    course = convert_course(course)
+    subject = course[0].upper()
+    number = course[1].upper()
+    section = section.upper()
+
+    grade_data = grades_db[term]
+    course_grade = grade_data.find_one(
+        {"subj": subject, "num": number, "sect": section})
+    del course_grade["_id"]
+    return course_grade
