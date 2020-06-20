338281a80d43346558458cc22bba985439cfac69
Anirudh Emmadi <kanna6501@gmail.com>
2020-06-20 18:41:20 -0500

    Added function to mass retrieve data for populating db

--- src/production/render.py
+++ src/production/render.py
@@ -1,7 +1,9 @@
 # Import Libraries
 import requests
-import re
 import sys
+
+# from .data import *
+from data import *
 
 # Renders raw markdown from README.md in github master branch into HTML
 
@@ -45,27 +47,23 @@ def get_grades(term, course, section):
                     return grade_data[i]
 
 
-# Converts course into an array seperating subject and number, math2413 -> [math, 2413]
+def grades_by_term(term):
+    # Format to match data json
+    term = convert_term(term)
+    # course = convert_course(course)
 
-def convert_course(course):
-    num = re.findall('\d+', course)[0]
-    subj = course.replace(f"{num}", "")
-    return [subj, num]
+    # Bad input
+    if(term == -1):
+        return 400
 
-# Converts term into expanded format, 18f -> Fall 2018
+    # Find json for term
+    url = f'https://api.github.com/repos/bharatari/utd-grades/contents/data/{term}?ref=master'
+    response = requests.get(url).json()
 
+    for raw_file in response:
+        if "json" in raw_file["name"]:
+            url = raw_file["download_url"]
 
-def convert_term(term):
-    season = term[-1]
-    term = f"20{term[:-1]}"
-
-    if season == "f":
-        season = "Fall"
-    elif season == "s":
-        season = "Spring"
-    elif season == "u":
-        season = "Summer"
-    else:
-        return -1
-
-    return f"{season}%20{term}"
+    # Grab json
+    grade_data = requests.get(url).json()
+    return grade_data
